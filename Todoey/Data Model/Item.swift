b52a92744b937e4d34618a23c6648c7c8bca0184
Ivan Gan <ivangan@ivans-macbook-air.local>
2018-01-22 17:42:08 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ivan Gan Hong Oon on 22/01/2018.
+//  Copyright © 2018 Naviworks Productions. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
+
+
